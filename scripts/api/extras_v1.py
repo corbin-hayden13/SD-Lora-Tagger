f1ecb1e9c8bade0936ba9d0909431498fe7e5b1d
Cruxial <benjamin@theones.dk>
2024-03-13 19:58:55 +0100

    Add placeholder button for civitai tag feature

--- scripts/api/extras_v1.py
+++ scripts/api/extras_v1.py
@@ -4,7 +4,9 @@ class ExtrasAPIv1(ExtrasAPI):
 
     def create(self, label='Extras') -> gr.Accordion:
         with super().create(label) as extras:
-            gr.Button("Add all models", variant="primary")
+            with gr.Row():
+                gr.Button("Add all models", variant="primary")
+                gr.Button("Add CivitAI Tags", variant="primary")
             
         
         return extras
